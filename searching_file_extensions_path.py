38d1cf742eef1759987a17cc99d1147465da57cf
sueliomatias <su_elio@hotmail.com>
2024-06-03 23:23:08 -0300

    searching_file_extensions_path

---
+++ searching_file_extensions_path.py
@@ -0,0 +1,13 @@
+
+import glob
+
+# diretório onde os arquivos serão pesquisados
+diretorio = '/var/log/'
+
+# pesquisar arquivos com a extensão .gz
+extensao = '*.gz'
+arquivos = glob.glob(diretorio + '/' + extensao)
+
+# exibir os arquivos encontrados
+for arquivo in arquivos:
+    print(arquivo)
